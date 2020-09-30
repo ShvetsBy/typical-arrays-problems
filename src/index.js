25b76e4e916f5a8b1322bfe98469eaa14bbd5321
Shvets Ivan <ishvets@MacBook-Pro-Shvets.local>
2020-09-30 12:19:04 +0300

    feat: min, max and avg solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    if (arguments[0] === undefined || array.length === 0) {
+    return 0;
+
+  } else {
+    
+      let min = array[0];
+
+      for (i = 0; i < array.length; ++i) {
+
+      if (array[i] < min) {
+        min = array[i];
+      } 
+     
+  }
+  return min;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+
+  if (arguments[0] === undefined || array.length === 0) {
+    return 0;
+
+  } else {
+    let max = array[0];
+
+    for (i = 0; i < array.length; ++i) {
+    
+    if (array[i] > max) {
+      max = array[i];
+    } 
+    
+  }
+  return max;
+}
+   
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments[0] === undefined || array.length === 0) {
+    return 0;
+  } else { 
+    let sum = 0;
+  for (i = 0; i < array.length; ++i) {
+    sum += array[i];
+    };
+
+  return (sum / array.length);
+}
 }
